ac1a9382663b8848888171c7dbacff85d398668a
SebasCastell <secasta26@gmail.com>
2023-05-22 23:12:46 -0500

    funciona en BD azure

--- app/db.py
+++ app/db.py
@@ -8,12 +8,12 @@ from app import app
 load_dotenv()  # take environment variables from .env.
 
 # Mysql Settings
-app.config['MYSQL_USER'] = os.getenv('MYSQL_USER') or 'root'
-app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD') or ''
-app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST') or 'localhost' # localhost
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER') or 'secasta' #secasta / root
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD') or 'Colegio123' #Colegio123 / ""
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST') or 'colegioudec.mysql.database.azure.com' # localhost
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB') or 'flaskcrud'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-app.config['MYSQL_PORT'] = 3307
+app.config['MYSQL_PORT'] = 3306
 
 # MySQL Connection
 mysql = MySQL(app)
